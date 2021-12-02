b6564c3f65f79f1fc8aca86625c8ad96aaf4f8c8
Sean Yang <seanyang@berkeley.edu>
2021-12-02 15:27:24 -0800

    update CORS

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use("/", router)
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://fullstack-final-frontend.vercel.app/"); 
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
   next();
 });
 
